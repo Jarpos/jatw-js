55113c80c97fd21abb8e40b5479d705dca96d26e
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-03 23:02:50 +0200

    ref: update help output a bit

--- docs/commands/help.js
+++ docs/commands/help.js
@@ -1,11 +1,13 @@
 /**
  * Prints help screen
  */
- function Help() {
+function Help() {
     const span = (key) =>
         `<span class="command" onclick=setCurrentInput("${key}")>${key}</span>`;
 
+    addLine();
     for (const [key, value] of commands.entries()) {
-        addLine(span(key), " ".repeat(10 - key.length), value.info);
+        addLine("    ", span(key), " ".repeat(15 - key.length), value.info);
     }
+    addLine();
 }
