d2a47db01dad4bb1ad11cc6aae09083756a75336
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-03 15:07:04 +0200

    feat: add ability to click on help output to autofill that command

--- docs/commands.js
+++ docs/commands.js
@@ -30,8 +30,11 @@ function getSuggestions(s) {
  * Prints help screen
  */
 function Help() {
+    const span = (key) =>
+        `<span class="command" onclick=setCurrentInput("${key}")>${key}</span>`;
+
     for (const [key, value] of commands.entries()) {
-        addLine(key, " ".repeat(10 - key.length), value.info);
+        addLine(span(key), " ".repeat(10 - key.length), value.info);
     }
 }
 
