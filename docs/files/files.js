375a1e9e04d6d674daf3b0277d03283e4cdaf481
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-04 21:48:33 +0200

    feat: add a few of my pictures to be viewable later

---
+++ docs/files/files.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * Files Map
+ * @TODO Add proper descriptions and more info
+ * @TODO Try to emulate a filesystem (semi) properly
+ */
+const files = new Map([
+    ["IMG_0701.png", {}],
+    ["IMG_0755.png", {}],
+    ["IMG_0776.png", {}],
+    ["IMG_0782.png", {}],
+    ["IMG_0847.png", {}],
+    ["IMG_0919.png", {}],
+    ["IMG_1011.png", {}],
+    ["IMG_1035.png", {}],
+    ["IMG_1055.png", {}],
+    ["IMG_1257.png", {}],
+]);
