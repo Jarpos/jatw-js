f4e0733d0f679dc9a76bb6f5b0089f87a0f77e6a
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-05 22:39:27 +0200

    ref: change files to be an array instead of a map (for now)

--- docs/files/files.js
+++ docs/files/files.js
@@ -6,15 +6,15 @@
  * @TODO Try to emulate a filesystem (semi) properly
  * @TODO Maybe make this an Array?
  */
-const files = new Map([
-    ["IMG_0701.png", {}],
-    ["IMG_0755.png", {}],
-    ["IMG_0776.png", {}],
-    ["IMG_0782.png", {}],
-    ["IMG_0847.png", {}],
-    ["IMG_0919.png", {}],
-    ["IMG_1011.png", {}],
-    ["IMG_1035.png", {}],
-    ["IMG_1055.png", {}],
-    ["IMG_1257.png", {}],
-]);
+const files = [
+    { name: "IMG_0701.png", },
+    { name: "IMG_0755.png", },
+    { name: "IMG_0776.png", },
+    { name: "IMG_0782.png", },
+    { name: "IMG_0847.png", },
+    { name: "IMG_0919.png", },
+    { name: "IMG_1011.png", },
+    { name: "IMG_1035.png", },
+    { name: "IMG_1055.png", },
+    { name: "IMG_1257.png", },
+];
