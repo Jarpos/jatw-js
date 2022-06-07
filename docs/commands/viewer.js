8add345cf914c925537bfb0ce2b916f29b6edf31
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-07 20:16:42 +0200

    ref: change rest of codebase to use img-viewer

--- docs/commands/viewer.js
+++ docs/commands/viewer.js
@@ -4,7 +4,7 @@
  * Opens new window with picture viewer
  */
 function Viewer() {
-    const viewer = document.createElement("d-window");
+    const viewer = document.createElement("d-img-viewer");
     viewer.setAttribute("name", "Picture Viewer");
     viewer.setAttribute("files", JSON.stringify(files));
     terminal().appendChild(viewer);
