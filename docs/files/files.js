84b0dd3fb48a3db36082c3280bf01bad378b8917
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-14 13:12:59 +0200

    ref: change file object creation to use function

--- docs/files/files.js
+++ docs/files/files.js
@@ -1,37 +1,50 @@
 "use strict";
 
 /**
+ * Create file object with name and type
+ * @param {string} name Name of file
+ * @param {"picture" | "text"} type Type of file
+ * @returns File object
+ */
+function MakeFile(name, type) {
+    return {
+        name: name,
+        type: type,
+    }
+}
+
+/**
  * Files Map
  * @TODO Add proper descriptions and more info
  * @TODO Try to emulate a filesystem (semi) properly
  * @TODO Maybe make this an Array?
  */
 const files = [
-    { name: "IMG_0121.jpg", },
-    { name: "IMG_0416.jpg", },
-    { name: "IMG_0428.jpg", },
-    { name: "IMG_0529.jpg", },
-    { name: "IMG_0531.jpg", },
-    { name: "IMG_0591.jpg", },
-    { name: "IMG_0593.jpg", },
-    { name: "IMG_0701.jpg", },
-    { name: "IMG_0755.jpg", },
-    { name: "IMG_0776.jpg", },
-    { name: "IMG_0782.jpg", },
-    { name: "IMG_0843.jpg", },
-    { name: "IMG_0847.jpg", },
-    { name: "IMG_0871.jpg", },
-    { name: "IMG_0919.jpg", },
-    { name: "IMG_0922.jpg", },
-    { name: "IMG_1005.jpg", },
-    { name: "IMG_1011.jpg", },
-    { name: "IMG_1035.jpg", },
-    { name: "IMG_1055.jpg", },
-    { name: "IMG_1080.jpg", },
-    { name: "IMG_1093.jpg", },
-    { name: "IMG_1257.jpg", },
-    { name: "IMG_1492.jpg", },
-    { name: "IMG_1586.jpg", },
-    { name: "IMG_1853.jpg", },
-    { name: "IMG_1906.jpg", },
+    MakeFile("IMG_0121.jpg", "picture"),
+    MakeFile("IMG_0416.jpg", "picture"),
+    MakeFile("IMG_0428.jpg", "picture"),
+    MakeFile("IMG_0529.jpg", "picture"),
+    MakeFile("IMG_0531.jpg", "picture"),
+    MakeFile("IMG_0591.jpg", "picture"),
+    MakeFile("IMG_0593.jpg", "picture"),
+    MakeFile("IMG_0701.jpg", "picture"),
+    MakeFile("IMG_0755.jpg", "picture"),
+    MakeFile("IMG_0776.jpg", "picture"),
+    MakeFile("IMG_0782.jpg", "picture"),
+    MakeFile("IMG_0843.jpg", "picture"),
+    MakeFile("IMG_0847.jpg", "picture"),
+    MakeFile("IMG_0871.jpg", "picture"),
+    MakeFile("IMG_0919.jpg", "picture"),
+    MakeFile("IMG_0922.jpg", "picture"),
+    MakeFile("IMG_1005.jpg", "picture"),
+    MakeFile("IMG_1011.jpg", "picture"),
+    MakeFile("IMG_1035.jpg", "picture"),
+    MakeFile("IMG_1055.jpg", "picture"),
+    MakeFile("IMG_1080.jpg", "picture"),
+    MakeFile("IMG_1093.jpg", "picture"),
+    MakeFile("IMG_1257.jpg", "picture"),
+    MakeFile("IMG_1492.jpg", "picture"),
+    MakeFile("IMG_1586.jpg", "picture"),
+    MakeFile("IMG_1853.jpg", "picture"),
+    MakeFile("IMG_1906.jpg", "picture"),
 ];
