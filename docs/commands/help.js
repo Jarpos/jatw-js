7fb09a6cade7a8d9fd0a7f6a9294b3b401e51218
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-15 10:04:56 +0200

    style: remove default parameter from padding

--- docs/commands/help.js
+++ docs/commands/help.js
@@ -7,13 +7,13 @@
 function Help(argv) {
     addLine();
     for (const [key, value] of commands.entries()) {
-        const line = addLine("    ", key.padEnd(15, " "), value.info);
+        const line = addLine("    ", key.padEnd(15), value.info);
         line.classList.add("nomark", "command");
         line.addEventListener("click", (e) => setAndExecuteCommand(key));
     }
     addLine();
-    addLine("    ", "[tab]".padEnd(15, " "), "Autocompletion");
-    addLine("    ", "[ctrl+c]".padEnd(15, " "), "Cancel current command");
-    addLine("    ", "[click]".padEnd(15, " "), "You can click on some stuff to execute/use it");
+    addLine("    ", "[tab]".padEnd(15), "Autocompletion");
+    addLine("    ", "[ctrl+c]".padEnd(15), "Cancel current command");
+    addLine("    ", "[click]".padEnd(15), "You can click on some stuff to execute/use it");
     addLine();
 }
