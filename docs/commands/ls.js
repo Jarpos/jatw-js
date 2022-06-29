98d3d80df86df45d5b87f8c10f004636e706ea8c
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-06-29 15:02:46 +0200

    ref: move cd out to its own file

--- docs/commands/ls.js
+++ docs/commands/ls.js
@@ -13,38 +13,3 @@ function Ls(argv) {
     }
     addLine(s);
 }
-
-/**
- * Changes directory
- * @param {string[]} argv Arguments
- */
-function Cd(argv) {
-    if (argv.length === 0) {
-        cwd = fileroot;
-    } else {
-        const path_fragments = argv[0].split("/");
-        console.log(path_fragments);
-        if (path_fragments[0] === "") {
-            cwd = fileroot;
-            path_fragments.shift();
-        }
-
-        for (const fragment of path_fragments) {
-            console.log(fragment);
-            switch (fragment) {
-                case ".": cwd = cwd; break;
-                case "..": cwd = cwd.parent ? cwd.parent : cwd; break;
-                case "": break;
-
-                default: {
-                    const folder = cwd.children.find(c => c.name === fragment);
-                    if (folder && folder.isDir()) {
-                        cwd = folder;
-                    } else {
-                        addLine("Error: Folder not found");
-                    }
-                }
-            }
-        }
-    }
-}
