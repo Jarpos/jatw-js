c2bc1c0304f1fc50e48824b4119b3714344b79ad
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-07-22 09:18:54 +0200

    ref: fix adding single characters to input

--- docs/components/InputLine.js
+++ docs/components/InputLine.js
@@ -60,7 +60,7 @@ class InputLine extends HTMLElement {
      * @param {string} input Input to append to the end
      */
     AddInput(input) {
-        this.left.push(input.split(""));
+        this.left.push(input.length > 1 ? input.split("") : input);
         this.Render();
     }
 
