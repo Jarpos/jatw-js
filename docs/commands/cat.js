c6bdb4b9da95840310c5b25d8dd5337816f818ec
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-07-27 20:36:50 +0200

    feat: add simple cat command

---
+++ docs/commands/cat.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Concatenate files and print on the standard output
+ * @param {string[]} argv Arguments
+ */
+function Cat(argv) {
+    if (argv?.length >= 1) {
+        const file = cwd.children.find(f => f.name === argv[0]);
+        if (file?.type === FILE_TYPE.TEXT) {
+            for (const line of file.content) {
+                addLine(line);
+            }
+        } else {
+            if (!file) {
+                addLine("Could not find file: ", argv[0]);
+            } else {
+                addLine(argv[0], " is not a textfile");
+            }
+        }
+    } else {
+        addLine("Error: you need to provide an argument");
+    }
+}
