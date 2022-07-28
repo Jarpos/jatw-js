a56ca482d92a7a0cc4e5bf503fe58bb6d0915171
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-07-28 15:54:02 +0200

    ref: use resolvePath to get the given path resolved

--- docs/commands/cd.js
+++ docs/commands/cd.js
@@ -8,27 +8,13 @@ function Cd(argv) {
     if (argv.length === 0) {
         cwd = fileroot;
     } else {
-        const path_fragments = argv[0].split("/");
-        if (path_fragments[0] === "") {
-            cwd = fileroot;
-            path_fragments.shift();
-        }
-
-        for (const fragment of path_fragments) {
-            switch (fragment) {
-                case ".": cwd = cwd; break;
-                case "..": cwd = cwd.parent ? cwd.parent : cwd; break;
-                case "": break;
-
-                default: {
-                    const folder = cwd.children.find(c => c.name === fragment);
-                    if (folder && folder.isDir()) {
-                        cwd = folder;
-                    } else {
-                        addLine("Error: Folder not found");
-                    }
-                }
-            }
+        const result = resolvePath(argv[0]);
+        if (!result) {
+            addLine("Error: Invalid path or could not find specified folder")
+        } else if (!result.isDir()) {
+            addLine("Error: File ", result.name, " is not a folder");
+        } else {
+            cwd = result;
         }
     }
 }
