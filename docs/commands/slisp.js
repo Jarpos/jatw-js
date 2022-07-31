62cb4ec67bf76f192592537cc18aadd90186edfa
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-07-31 03:56:41 +0200

    ref: update to set the inputhandler and redo it minorly

--- docs/commands/slisp.js
+++ docs/commands/slisp.js
@@ -1,25 +1,36 @@
 "use strict";
 
 function sLisp(argv) {
-    addLine("**************************************");
-    addLine("* Simple List Processing Interpreter *");
-    addLine("*                                    *");
-    addLine("* This is just a simple interpreter  *");
-    addLine("* inspired by the lisp programming   *");
-    addLine("* language and this blog article:    *");
-    addLine("* https://stopa.io/post/265          *");
-    addLine("*                                    *");
-    addLine("* See slisp --help for more info     *");
-    addLine("**************************************");
+    addLine("******************************************");
+    addLine("*                                        *");
+    addLine("*   Simple List Processing Interpreter   *");
+    addLine("*                                        *");
+    addLine("*   This is just a simple interpreter    *");
+    addLine("*   inspired by the lisp programming     *");
+    addLine("*   language and this blog article:      *");
+    addLine("*   https://stopa.io/post/265            *");
+    addLine("*                                        *");
+    addLine("*   See slisp --help for more info       *");
+    addLine("*                                        *");
+    addLine("******************************************");
     addLine();
+    enterhandler = sLispEnterHandler;
+}
 
-    let input;
-    while ((input = currentline.Input) !== "(exit)") {
+/**
+ * The enter/input handler
+ * @param {KeyboardEvent} e KeyboardEvent to process
+ */
+function sLispEnterHandler(e) {
+    const input = currentline.Input.replace(/ +/, ", ");
+    newCurrentline();
+
+    if (input === "(exit)") {
+        enterhandler = defaultEnterHandler;
     }
+    // evaluateSLispExpression(parseSLispExpression(input));
 }
 
-function parseLispExpression(input) {
-}
+function parseSLispExpression(input) { }
 
-function evaluateSLispExpression(expression) {
-}
+function evaluateSLispExpression(expression) { }
