856ab108d32bb181c8374a22afe27dc5b01472b8
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-08-06 02:43:55 +0200

    style: ref disclaimer on opening slisp interpreter

--- docs/commands/slisp.js
+++ docs/commands/slisp.js
@@ -8,18 +8,18 @@
 function sLisp(argv) {
     if (argv.length === 0) {
         addLine();
-        addLine("******************************************");
-        addLine("*                                        *");
-        addLine("*   Simple List Processing Interpreter   *");
-        addLine("*                                        *");
-        addLine("*   This is just a simple interpreter    *");
-        addLine("*   inspired by the lisp programming     *");
-        addLine("*   language and this blog article:      *");
-        addLine("*   https://stopa.io/post/265            *");
-        addLine("*                                        *");
-        addLine("*   See slisp --help for more info       *");
-        addLine("*                                        *");
-        addLine("******************************************");
+        addLine("* * * * * * * * * * * * * * * * * * * * * *");
+        addLine("*                                         *");
+        addLine("*   Simple List Processing Interpreter    *");
+        addLine("*                                         *");
+        addLine("*   This is just a simple interpreter     *");
+        addLine("*   inspired by the lisp programming      *");
+        addLine("*   language and this blog article:       *");
+        addLine("*   https://stopa.io/post/265             *");
+        addLine("*                                         *");
+        addLine("*   See slisp --help for more info        *");
+        addLine("*                                         *");
+        addLine("* * * * * * * * * * * * * * * * * * * * * *");
         enterhandler = sLispEnterHandler;
         inputlineinfohandler = () => "> ";
         newCurrentline();
