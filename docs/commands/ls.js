a8775d2c3dee690b87abaf450894714e1fb12d21
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-09-02 15:19:30 +0200

    feat: add ls arg for specifying folder

--- docs/commands/ls.js
+++ docs/commands/ls.js
@@ -5,22 +5,28 @@
  * @param {string[]} argv Arguments
  */
 function Ls(argv) {
-    let listed = addLine();
-    listed.innerHTML = "";
-    for (let i = 0; i < cwd.children.length; i++) {
-        const item = document.createElement("span");
-        item.innerText = cwd.children[i].name + "     ";
-        listed.append(item);
+    const folder = argv.length > 0 ? resolvePath(argv[0]) : cwd;
 
-        if (cwd.children[i].isDir()) {
-            item.setAttribute("class", "ls-folder")
-        } else {
-            item.setAttribute("class", "ls-file")
-        }
+    if (folder) {
+        let listed = addLine();
+        listed.innerHTML = "";
+        for (let i = 0; i < folder.children.length; i++) {
+            const item = document.createElement("span");
+            item.innerText = folder.children[i].name + "     ";
+            listed.append(item);
 
-        if ((i - 4) % 5 === 0) {
-            listed = addLine();
-            listed.innerHTML = "";
+            if (folder.children[i].isDir()) {
+                item.setAttribute("class", "ls-folder")
+            } else {
+                item.setAttribute("class", "ls-file")
+            }
+
+            if ((i - 4) % 5 === 0) {
+                listed = addLine();
+                listed.innerHTML = "";
+            }
         }
+    } else {
+        addLine("Error: Could not find foler ", argv[0]);
     }
 }
