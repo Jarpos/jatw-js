e605c1172bac5fc3c5336241ecf39543e760d9d5
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-08-21 21:49:34 +0200

    style: update names; change comments

--- src/helpers/wasm.js
+++ src/helpers/wasm.js
@@ -3,7 +3,7 @@ import { addLine } from "../helpers.js";
 /**
  * Options for the imported Wasm modules
  */
-const importObject = {
+const factorizeObject = {
     module: {},
     imports: {},
     env: {
@@ -15,12 +15,11 @@ const importObject = {
          * @param {ui64} prime Prime to add to the output primes
          * @returns updated primes string
          */
-        UpdateLine: (prime) => importObject.env.primes += `${prime} `,
+        UpdateLine: (prime) => factorizeObject.env.primes += `${prime} `,
 
         /**
-         * See:
-         * https://emscripten.org/docs/api_reference/emscripten.h.html#c.emscripten_notify_memory_growth
          * @param {i32} index Executed when memory is grown
+         * @link https://emscripten.org/docs/api_reference/emscripten.h.html#c.emscripten_notify_memory_growth
          */
         emscripten_notify_memory_growth: (index) => { },
         memory: new WebAssembly.Memory({ initial: 2 ** 16, }),
@@ -30,9 +29,9 @@ const importObject = {
 /**
  * Exported functions from Wasm
  */
-const exports =
+const factorize_c =
     await WebAssembly
-        .instantiateStreaming(fetch('helpers/factorize.c.wasm'), importObject)
+        .instantiateStreaming(fetch('helpers/factorize.c.wasm'), factorizeObject)
         .then(result => result.instance.exports);
 
 /**
@@ -45,10 +44,10 @@ export const Wasm = {
      * @returns {void} Nothing
      */
     Factorize: (number) => {
-        importObject.env.primes = "";
-        exports.Factorize(BigInt(number));
+        factorizeObject.env.primes = "";
+        factorize_c.Factorize(BigInt(number));
 
         addLine("Factorizing: ", BigInt(number));
-        addLine(importObject.env.primes);
+        addLine(factorizeObject.env.primes);
     },
 };
