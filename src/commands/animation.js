068fa35a0cedb2423084bf18c8f69a1f08506107
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-08-27 01:41:02 +0200

    fix: add name to animation window

--- src/commands/animation.js
+++ src/commands/animation.js
@@ -6,11 +6,14 @@ import { Wasm } from "../helpers/wasm.js";
  * @param {string[]} argv Arguments
  */
 export function Animation(argv) {
-    const window = document.createElement("d-window");
     const canvas = document.createElement("canvas");
     canvas.width = 640;
     canvas.height = 640;
+
+    const window = document.createElement("d-window");
+    window.setAttribute("name", "Animation");
     window.shadowRoot.appendChild(canvas);
+
     Wasm.Animation(canvas);
     terminal().appendChild(window);
 }
