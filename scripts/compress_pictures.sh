215630aa3a5590074f6a482132a59c91c54076a4
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-09-07 23:40:07 +0200

    add script for adding and compressing pictures

---
+++ scripts/compress_pictures.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+script_dir=$(dirname "$0")
+if [ "$script_dir" = '.' ]; then
+    script_dir=$(pwd)
+fi
+
+cd "$script_dir/../src/files/pictures" || exit
+
+for i in *.png; do
+    o=${i%.png}.jpg
+
+    echo "$i -> $o"
+    convert $i -define jpeg:extent=175kb $o
+done
+
+rm *.png
+
+echo
+echo "Add these files to files.js:"
+
+for f in *.jpg; do
+    echo "new File_c(FILE_TYPE.PICTURE, \"$f\", \"files/pictures/$f\"),"
+done
