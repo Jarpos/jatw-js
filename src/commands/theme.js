7732384e076f10f4b66709a27fed6cb7939dedf9
Jarpos <51173894+Jarpos@users.noreply.github.com>
2023-09-07 13:55:34 +0200

    style: update comments, move definitions

--- src/commands/theme.js
+++ src/commands/theme.js
@@ -9,47 +9,53 @@ import { styleroot } from "../globals.js";
  */
 export function Theme(argv) {
     /**
-     * Creates a theme object
+     * `Create`s a theme object
      * @param {string} n Name
-     * @param {string} f Foreground
-     * @param {string} b Background
-     * @param {string} u Username
-     * @param {string} h Hostname
+     * @param {number} f Foreground
+     * @param {number} b Background
+     * @param {number} u Username
+     * @param {number} h Hostname
      * @returns Theme object
      */
-    const ct = (n, f, b, u, h) => {
+    const c = (n, f, b, u, h) => {
         return { name: n, fg: f, bg: b, uname: u, hname: h, };
     };
-
-    /**
-     * Sets to given theme object
-     * @param {Theme} t Theme to set to
-     */
-    const setTheme = (t) => {
-        styleroot().setProperty("--bg-color", "#" + t.bg.toString(16).padStart(6, 0));
-        styleroot().setProperty("--fg-color", "#" + t.fg.toString(16).padStart(6, 0));
-        styleroot().setProperty("--username", "#" + t.uname.toString(16).padStart(6, 0));
-        styleroot().setProperty("--hostname", "#" + t.hname.toString(16).padStart(6, 0));
-        document.location.hash = t.name;
-    }
 
     /**
      * Array with defined themes
      * @TODO Maybe make this a map? Then global and save between pageloads?
      */
     const themes = [
-        /* Name -------------- fg ------ bg ------ uname --- hname - */
-        ct("default", /******/ 0xc7ccd1, 0x1c2023, 0x95c7ae, 0xae95c7),
-        ct("alternative", /**/ 0x9ea7a6, 0x232c31, 0xa03b1e, 0x3643b4),
-        ct("hekr", /*********/ 0x00ff00, 0x000000, 0x00ff00, 0x00ff00),
-        ct("t-pride", /******/ 0xcccccc, 0x1c2023, 0x5bcefa, 0xf5a9b8),
-        ct("nb-pride", /*****/ 0xffffff, 0x111111, 0x9a5acf, 0xfff333),
-        ct("gq-pride", /*****/ 0xcccccc, 0x1c2023, 0x498022, 0xb57edc),
-        ct("bi-pride", /*****/ 0x9b4f96, 0x1c2023, 0xd60270, 0x006bdb),
-        ct("l-pride", /******/ 0xd461a6, 0x200010, 0xd62900, 0xff9b55),
-        ct("g-pride", /******/ 0x7bade3, 0x060823, 0x26ceaa, 0x5049cb),
-        ct("light", /********/ 0x303030, 0xfefefe, 0x75b5aa, 0xaa759f),
+        /* Name ------------- fg ------ bg ------ uname --- hname - */
+        c("default", /******/ 0xc7ccd1, 0x1c2023, 0x95c7ae, 0xae95c7),
+        c("alternative", /**/ 0x9ea7a6, 0x232c31, 0xa03b1e, 0x3643b4),
+        c("hekr", /*********/ 0x00ff00, 0x000000, 0x00ff00, 0x00ff00),
+        c("t-pride", /******/ 0xcccccc, 0x1c2023, 0x5bcefa, 0xf5a9b8),
+        c("nb-pride", /*****/ 0xffffff, 0x111111, 0x9a5acf, 0xfff333),
+        c("gq-pride", /*****/ 0xcccccc, 0x1c2023, 0x498022, 0xb57edc),
+        c("bi-pride", /*****/ 0x9b4f96, 0x1c2023, 0xd60270, 0x006bdb),
+        c("l-pride", /******/ 0xd461a6, 0x200010, 0xd62900, 0xff9b55),
+        c("g-pride", /******/ 0x7bade3, 0x060823, 0x26ceaa, 0x5049cb),
+        c("light", /********/ 0x303030, 0xfefefe, 0x75b5aa, 0xaa759f),
     ];
+
+    /**
+     * Sets to given theme object
+     * @param {{
+     *     name: string;
+     *     fg: number;
+     *     bg: number;
+     *     uname: number;
+     *     hname: number;
+     * }} t Theme to set to
+     */
+    const setTheme = (t) => {
+        document.location.hash = t.name;
+        styleroot().setProperty("--bg-color", "#" + t.bg.toString(16).padStart(6, 0));
+        styleroot().setProperty("--fg-color", "#" + t.fg.toString(16).padStart(6, 0));
+        styleroot().setProperty("--username", "#" + t.uname.toString(16).padStart(6, 0));
+        styleroot().setProperty("--hostname", "#" + t.hname.toString(16).padStart(6, 0));
+    };
 
     if (argv?.length >= 1) {
         const theme = themes.find(e => e.name === argv[0]);
